f0877bbce9ca338e8e622dac885647ef97c98616
shivang-sharma <sharmashivang071@gmail.com>
2023-03-22 14:41:53 +0530

    Updated the latest code

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
+import time
 from tkinter import *
-import pyautogui
-
+from PIL import ImageGrab
+import cv2
+import numpy as np
 import datetime
 
 
 def take_bounded_screenshot(x1, y1, x2, y2):
-    image = pyautogui.screenshot(region=(x1, y1, x2, y2))
+    image = np.array(ImageGrab.grab(bbox=(x1, y1, x2, y2)))
     file_name = datetime.datetime.now().strftime("%f")
-    image.save("snips/" + file_name + ".png")
+    # image.save("snips/" + file_name + ".png")
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imwrite("snips/" + file_name + ".png", image)
 
 
 class Application:
-    def __init__(self, master):
+    def __init__(self, master, capture_coordinates):
         self.snip_surface = None
         self.master = master
         self.start_x = None
         self.start_y = None
         self.current_x = None
         self.current_y = None
-
+        self.capture_coordinates = capture_coordinates
         root.geometry('400x50+200+200')  # set new geometry
         root.title('Lil Snippy')
 
@@ -28,20 +32,32 @@ class Application:
         self.buttonBar = Frame(self.menu_frame, bg="")
         self.buttonBar.pack()
 
-        self.snipButton = Button(self.buttonBar, width=5, height=5, command=self.create_screen_canvas, background="green")
+        self.snipButton = Button(self.buttonBar, width=5, height=5, command=self.create_screen_canvas, text="Snip")
         self.snipButton.pack()
+
+        self.captureShotButton = Button(self.buttonBar, width=5, height=5, command=self.capture_shot,
+                                        text="Capture")
 
         self.master_screen = Toplevel(root)
         self.master_screen.withdraw()
-        self.master_screen.attributes("-alpha", 0.0)
-        self.picture_frame = Frame(self.master_screen, background="maroon3")
+        self.picture_frame = Frame(self.master_screen)
         self.picture_frame.pack(fill=BOTH, expand=YES)
+
+    def capture_shot(self):
+        root.withdraw()
+        time.sleep(1)
+        take_bounded_screenshot(
+            self.capture_coordinates[0], self.capture_coordinates[1],
+            self.capture_coordinates[2], self.capture_coordinates[3])
+        self.captureShotButton.pack_forget()
+        self.snipButton.pack()
+        root.deiconify()
 
     def create_screen_canvas(self):
         self.master_screen.deiconify()
         root.withdraw()
 
-        self.snip_surface = Canvas(self.picture_frame, cursor="cross", bg="grey11")
+        self.snip_surface = Canvas(self.picture_frame, cursor="cross")
         self.snip_surface.pack(fill=BOTH, expand=YES)
 
         self.snip_surface.bind("<ButtonPress-1>", self.on_button_press)
@@ -49,30 +65,48 @@ class Application:
         self.snip_surface.bind("<ButtonRelease-1>", self.on_button_release)
 
         self.master_screen.attributes('-fullscreen', True)
-        self.master_screen.attributes('-alpha', .3)
+        self.master_screen.attributes('-alpha', .1)
         self.master_screen.lift()
         self.master_screen.attributes("-topmost", True)
 
     def on_button_release(self, event):
         self.display_rectangle_position()
-
         if self.start_x <= self.current_x and self.start_y <= self.current_y:
             print("right down")
             take_bounded_screenshot(self.start_x, self.start_y, self.current_x - self.start_x, self.current_y - self.start_y)
+            self.capture_coordinates.append(self.start_x)
+            self.capture_coordinates.append(self.start_y)
+            self.capture_coordinates.append(self.current_x - self.start_x)
+            self.capture_coordinates.append(self.current_y - self.start_y)
 
         elif self.start_x >= self.current_x and self.start_y <= self.current_y:
             print("left down")
             take_bounded_screenshot(self.current_x, self.start_y, self.start_x - self.current_x, self.current_y - self.start_y)
+            self.capture_coordinates.append(self.current_x)
+            self.capture_coordinates.append(self.start_y)
+            self.capture_coordinates.append(self.start_x - self.current_x)
+            self.capture_coordinates.append(self.current_y - self.start_y)
 
         elif self.start_x <= self.current_x and self.start_y >= self.current_y:
             print("right up")
             take_bounded_screenshot(self.start_x, self.current_y, self.current_x - self.start_x, self.start_y - self.current_y)
+            self.capture_coordinates.append(self.start_x)
+            self.capture_coordinates.append(self.current_y)
+            self.capture_coordinates.append(self.current_x - self.start_x)
+            self.capture_coordinates.append(self.start_y - self.current_y)
 
         elif self.start_x >= self.current_x and self.start_y >= self.current_y:
             print("left up")
             take_bounded_screenshot(self.current_x, self.current_y, self.start_x - self.current_x, self.start_y - self.current_y)
+            self.capture_coordinates.append(self.current_x)
+            self.capture_coordinates.append(self.current_y)
+            self.capture_coordinates.append(self.start_x - self.current_x)
+            self.capture_coordinates.append(self.start_y - self.current_y)
 
         self.exit_screenshot_mode()
+        if len(self.capture_coordinates) == 4:
+            self.captureShotButton.pack()
+            self.snipButton.pack_forget()
         return event
 
     def exit_screenshot_mode(self):
@@ -84,7 +118,7 @@ class Application:
         # save mouse drag start position
         self.start_x = self.snip_surface.canvasx(event.x)
         self.start_y = self.snip_surface.canvasy(event.y)
-        self.snip_surface.create_rectangle(0, 0, 1, 1, outline='red', width=3, fill="maroon3")
+        self.snip_surface.create_rectangle(0, 0, 1, 1, outline='red', width=3)
 
     def on_snip_drag(self, event):
         self.current_x, self.current_y = (event.x, event.y)
@@ -99,6 +133,8 @@ class Application:
 
 
 if __name__ == '__main__':
+    output = list()
     root = Tk()
-    app = Application(root)
+    app = Application(root, output)
     root.mainloop()
+    print(output)
